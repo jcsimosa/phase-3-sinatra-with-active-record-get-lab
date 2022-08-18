59b97213abbe6422d55d4aca6b994f5362e1e9e5
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-08-18 15:57:20 -0500

    commit tree c0aa80e2993dd9ba8c77486af4ff2016daf60d95

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,21 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  # add routes
-
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+  get "/bakeries/:id" do
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json
+  end
+  get "/baked_goods/by_price" do
+    baked_goods = BakedGood.all.order(:price) 
+    baked_goods.to_json
+  end
+  get "/baked_goods/most_expensive" do
+    baked_goods = BakedGood.all.order(:price).reverse
+    baked_goods.to_json
+  end
 end
+
